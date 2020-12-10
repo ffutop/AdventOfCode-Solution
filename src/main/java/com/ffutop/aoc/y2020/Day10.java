d95d072128c925f2fd9d8434d008efe2a2159c2b
ffutop <17816873960@163.com>
2020-12-10 20:31:14 +0800

    feat: solution of day10

---
+++ src/main/java/com/ffutop/aoc/y2020/Day10.java
@@ -0,0 +1,52 @@
+package com.ffutop.aoc.y2020;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class Day10 extends BasicDay {
+
+    public static void main(String[] args) {
+        Day10 day10 = new Day10();
+        System.out.println(day10.solve1());
+        System.out.println(day10.solve2());
+    }
+
+    private int solve1() {
+        List<Integer> list = readLists(s -> Integer.valueOf(s));
+        list.sort((x, y) -> x-y);
+        int[] diffs = new int[4];
+        int prev = 0;
+        for (Integer val : list) {
+            diffs[val-prev]++;
+            prev = val;
+        }
+        return diffs[1] * (diffs[3]+1);
+    }
+
+    private long solve2() {
+        List<Integer> list = readLists(s -> Integer.valueOf(s));
+        list.add(0);
+        list.sort((x, y) -> x-y);
+        int outlet = list.get(list.size()-1) + 3;
+        list.add(outlet);
+        Map<Integer, Long> map = new HashMap<>();
+        map.put(0, 1L);
+        for (Integer val : list) {
+            final long count = map.get(val);
+            for (int i=1;i<=3;i++) {
+                map.compute(val + i, new BiFunction<Integer, Long, Long>() {
+                    @Override
+                    public Long apply(Integer key, Long oldValue) {
+                        if (oldValue == null) {
+                            oldValue = 0L;
+                        }
+                        return oldValue + count;
+                    }
+                });
+            }
+        }
+        return map.get(outlet);
+    }
+}
